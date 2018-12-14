d26e6bf365f7935bb1cdba006e714c33ba58625a
晋毓麟 <2522471070@qq.com>
2018-12-14 18:34:46 +0800

    create contents files

--- test/test.cpp
+++ test/test.cpp
@@ -1,40 +1,23 @@
-#include<iostream>
-#include<cstring>
+#include<bits/stdc++.h>
 using namespace std;
+
+void print_subset(int n,int * A,int cur)
+{
+    for (int i = 0;i<cur;i++)
+        printf("%d ",A[i]);
+    cout<<endl;
+    int s = cur?A[cur-1]+1 : 0;
+    for (int i = s;i < n;i++)
+    {
+        A[cur] = i;
+        print_subset(n,A,cur+1);
+    }
+}
+
 int main()
 {
-    char str[110],temp[110];
-    int n,a[30];
-    while (cin>>n){
-        memset(a,0,sizeof(a));
-        memset(temp,'\0',sizeof(temp));
-        cin>>str;
-        int len=strlen(str),flag=1;
-        for (int i=0;i<len;i++){
-            a[str[i]-'a'+1]++;
-        }
-        int cnt=0;
-        for (int i=1;i<=26&&flag;i++){
-            if (a[i]%n!=0){
-                flag=0;
-                break;
-            }
-            else{
-                int t=a[i]/n;
-                while (t--){
-                    temp[cnt++]=(char)('a'+i-1);
-                }
-            }
-        }
-        if (flag==1){
-//          int t=len/n;
-            while (n--){
-                printf("%s",temp);
-            }
-            printf("\n");
-        }
-        else
-            printf("-1\n");
-    }
+    int data[] = {1,2,7,4};
+    print_subset(4,data,0);
+
     return 0;
 }
